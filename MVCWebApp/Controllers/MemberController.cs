315cb835eb46832e872ab6d8c872918ee55ea18b
Aidan Killeen <aidan.killeen@gmail.com>
2023-12-01 11:07:12 +0000

    Web app with forms for add and delete

--- MVCWebApp/Controllers/MemberController.cs
+++ MVCWebApp/Controllers/MemberController.cs
@@ -17,5 +17,51 @@ namespace MVCWebApp.Controllers
 
             return View();
         }
+
+        public IActionResult Detail(int id)
+        {
+            ViewBag.Id = id;
+            MemberDao.MemberDao dao = new MemberDao.MemberDao();
+            var member = dao.GetMember(id);
+            dao.Close();
+            ViewBag.Member = member;
+            return View();
+        }
+
+        public IActionResult ConfirmDelete(int id)
+        {
+            ViewBag.Message = $"Are you sure you want to delete {id}?";
+            ViewBag.Id = id;
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            MemberDao.MemberDao dao = new MemberDao.MemberDao();
+
+            dao.DeleteMember(id);
+            dao.Close();
+
+            ViewBag.Message = $"Member { id } deleted successfully";
+            //return View();
+
+            return Redirect("/Member");
+        }
+
+        public IActionResult Add()
+        {
+            return View("MemberForm");
+        }
+
+        public IActionResult DoAdd(Member member)
+        {
+
+            MemberDao.MemberDao dao = new MemberDao.MemberDao();
+
+            dao.AddMember(member);
+            dao.Close();
+
+            return Redirect("/Member");
+        }
     }
 }
